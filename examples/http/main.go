3cf2d2b6f60f21d3e004797c724cdb2f78c7d067
Tom Anderson <dante.renevo@gmail.com>
2022-07-14 22:01:41 -0700

    Updated example with hosting from fs.FS or directory

--- examples/http/main.go
+++ examples/http/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"embed"
+	"flag"
+	"io/fs"
 	"net/http"
+	"os"
 
 	"github.com/renevo/bootstrap"
 )
 
-//go:embed index.html
+//go:embed static/***
 var content embed.FS
 
 func main() {
-	if err := bootstrap.HTTP("test", "0.0.0", http.FS(content)); err != nil {
+	flag.Parse()
+
+	var hfs http.FileSystem
+	if staticPath := os.Getenv("HTTP_STATIC_PATH"); staticPath != "" {
+		hfs = http.Dir(staticPath)
+	} else {
+		sfs, _ := fs.Sub(content, "static")
+		hfs = http.FS(sfs)
+	}
+
+	if err := bootstrap.HTTP("test", "0.0.0", hfs); err != nil {
 		panic(err)
 	}
 }
