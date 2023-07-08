683311c119473f01b6dddcfd46c080974a95f5ab
Tom Anderson <dante.renevo@gmail.com>
2023-07-08 15:06:09 -0700

    Extending http module support
    This adds the ability for other modules to extend the routes
    
    Adds in prometheus metrics endpoint with otel metrics support
    
    Fixed the server header to now be the app/version instead of Renevo/1.0.0 static
    
    properly shutting down/flushing otel on stop
    
    updated all dependencies

--- go.mod
+++ go.mod
@@ -3,40 +3,53 @@ module github.com/renevo/bootstrap
 go 1.18
 
 require (
-	github.com/caarlos0/env/v6 v6.9.3
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/pkg/errors v0.9.1
-	github.com/portcullis/application v0.0.0-20220609171158-fd56b7f9ad5f
+	github.com/portcullis/application v0.0.0-20230312190325-640f2dddbc87
 	github.com/portcullis/config v0.1.0
 	github.com/portcullis/logging v0.1.1
-	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.33.0
-	go.opentelemetry.io/otel v1.8.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.8.0
-	go.opentelemetry.io/otel/sdk v1.8.0
-	google.golang.org/grpc v1.48.0
+	github.com/prometheus/client_golang v1.16.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.42.0
+	go.opentelemetry.io/otel v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.16.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.39.0
+	go.opentelemetry.io/otel/metric v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
+	go.opentelemetry.io/otel/sdk/metric v0.39.0
+	google.golang.org/grpc v1.56.2
 )
 
 require (
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
-	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3 // indirect
-	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.17.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
-	github.com/zclconf/go-cty v1.10.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.8.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.8.0 // indirect
-	go.opentelemetry.io/otel/trace v1.8.0 // indirect
-	go.opentelemetry.io/proto/otlp v0.18.0 // indirect
-	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
-	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220714211235-042d03aeabc9 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.0 // indirect
+	github.com/zclconf/go-cty v1.13.2 // indirect
+	github.com/zclconf/go-cty-yaml v1.0.3 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.20.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- modules/otel/module.go
+++ modules/otel/module.go
@@ -2,12 +2,16 @@ package otel
 
 import (
 	"context"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/portcullis/application"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
@@ -16,6 +20,10 @@ import (
 )
 
 type module struct {
+	metricExporter *prometheus.Exporter
+	spanProcessor  sdktrace.SpanProcessor
+	traceExporter  *otlptrace.Exporter
+
 	cfg struct {
 		Addr string `env:"OTEL_GRPC_ADDRESS"`
 	}
@@ -36,10 +44,6 @@ func (m *module) Start(ctx context.Context) error {
 		return nil
 	}
 
-	if m.cfg.Addr == "" {
-		return nil
-	}
-
 	res, err := resource.New(ctx,
 		resource.WithAttributes(
 			// the service name used to display traces in backends
@@ -47,37 +51,68 @@ func (m *module) Start(ctx context.Context) error {
 			semconv.ServiceVersionKey.String(app.Version),
 		),
 	)
-
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize otel resource")
 	}
 
-	conn, err := grpc.DialContext(ctx, m.cfg.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-
+	// metrics
+	metricExporter, err := prometheus.New()
 	if err != nil {
-		return errors.Wrapf(err, "failed to connect to %q grpc collector", m.cfg.Addr)
+		return errors.Wrap(err, "failed to create otel prometheus exporter")
 	}
+	m.metricExporter = metricExporter
 
-	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
-	if err != nil {
-		return errors.Wrapf(err, "failed to create grpc trace exporter")
+	otel.SetMeterProvider(metric.NewMeterProvider(metric.WithReader(m.metricExporter), metric.WithResource(res)))
+
+	// tracing
+	if m.cfg.Addr != "" {
+		conn, err := grpc.DialContext(ctx, m.cfg.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+
+		if err != nil {
+			return errors.Wrapf(err, "failed to connect to %q grpc collector", m.cfg.Addr)
+		}
+
+		traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+		if err != nil {
+			return errors.Wrapf(err, "failed to create grpc trace exporter")
+		}
+		m.traceExporter = traceExporter
+
+		// Register the trace exporter with a TracerProvider, using a batch span processor to aggregate spans before export.
+		m.spanProcessor = sdktrace.NewBatchSpanProcessor(m.traceExporter)
+		tracerProvider := sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithResource(res),
+			sdktrace.WithSpanProcessor(m.spanProcessor),
+		)
+		otel.SetTracerProvider(tracerProvider)
+
+		// set global propagator to tracecontext (the default is no-op).
+		otel.SetTextMapPropagator(propagation.TraceContext{})
 	}
-
-	// Register the trace exporter with a TracerProvider, using a batch span processor to aggregate spans before export.
-	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
-	tracerProvider := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithResource(res),
-		sdktrace.WithSpanProcessor(bsp),
-	)
-	otel.SetTracerProvider(tracerProvider)
-
-	// set global propagator to tracecontext (the default is no-op).
-	otel.SetTextMapPropagator(propagation.TraceContext{})
 
 	return nil
 }
 
 func (m *module) Stop(ctx context.Context) error {
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// shutdown span processor
+	if m.spanProcessor != nil {
+		_ = m.spanProcessor.ForceFlush(shutdownCtx)
+		_ = m.spanProcessor.Shutdown(shutdownCtx)
+	}
+
+	// shutdown trace exporter
+	if m.traceExporter != nil {
+		_ = m.traceExporter.Shutdown(shutdownCtx)
+	}
+
+	// shutdown metric exporter
+	if m.metricExporter != nil {
+		_ = m.metricExporter.ForceFlush(shutdownCtx)
+		_ = m.metricExporter.Shutdown(shutdownCtx)
+	}
 	return nil
 }
