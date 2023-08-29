587109034fd1b6257621ebfc93e0069e9088e508
Tom Anderson <dante.renevo@gmail.com>
2023-08-29 14:44:46 -0700

    Logging refactor to remove portcullis/logging
    Removed pkg/errors, updated caarlos0/env to v9, as well as all other deps updated to latest

--- examples/http/application.hcl
+++ examples/http/application.hcl
@@ -1,3 +1,8 @@
 http "127.0.0.1:8080" {
-  shutdown_timeout = "5s"
+  read_timeout     = "5s"
+  write_timeout    = "10s"
+  idle_timeout     = "2m"
+  shutdown_timeout = "30s"
+  cert_file        = ""
+  key_file         = ""
 }
--- http.go
+++ http.go
@@ -3,12 +3,14 @@ package bootstrap
 import (
 	"context"
 	"flag"
+	"log/slog"
+	gohttp "net/http"
 	"os"
 
-	gohttp "net/http"
-
+	"github.com/lmittmann/tint"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 	"github.com/portcullis/application"
-	"github.com/portcullis/application/modules/logging"
 	"github.com/renevo/bootstrap/modules/env"
 	"github.com/renevo/bootstrap/modules/http"
 	"github.com/renevo/bootstrap/modules/otel"
@@ -16,12 +18,6 @@ import (
 
 // HTTP bootstraps a new http application and runs it
 func HTTP(name, version string, content gohttp.FileSystem, opts ...application.Option) error {
-	app := &application.Application{
-		Name:       name,
-		Version:    version,
-		Controller: &application.Controller{},
-	}
-
 	// initialize flags before constructing modules to allow them to register config
 	// see if any flags have been added to the default flagset
 	var hasFlags bool
@@ -33,14 +29,11 @@ func HTTP(name, version string, content gohttp.FileSystem, opts ...application.O
 		flag.CommandLine = flag.NewFlagSet(name, flag.ExitOnError)
 	}
 
-	// in built modules
-	app.Controller.Add("Logging", logging.New())
-	app.Controller.Add("Environment", env.New("", map[string]string{}))
-	app.Controller.Add("Telemetry", otel.New())
-	app.Controller.Add("HTTP", http.New(content))
-
 	// global application flags
 	cfgFile := flag.CommandLine.String("config", "", "Application configuration file")
+	debugFlag := flag.CommandLine.Bool("debug", false, "Enable application debug logging output")
+	jsonFlag := flag.CommandLine.Bool("json", false, "Enable JSON logging output")
+	noColorFlag := flag.CommandLine.Bool("no-color", false, "Disable colorized output on text")
 
 	// parse them
 	if !flag.CommandLine.Parsed() {
@@ -48,6 +41,41 @@ func HTTP(name, version string, content gohttp.FileSystem, opts ...application.O
 			return err
 		}
 	}
+
+	// logger setup
+	var logLeveler slog.LevelVar
+	var logHandler slog.Handler
+	logOutput := os.Stdout
+
+	switch {
+	case *jsonFlag:
+		logHandler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: &logLeveler})
+	case *noColorFlag:
+		logHandler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: &logLeveler})
+	default:
+		logHandler = tint.NewHandler(colorable.NewColorable(logOutput), &tint.Options{
+			Level:   &logLeveler,
+			NoColor: !isatty.IsTerminal(logOutput.Fd()),
+		})
+	}
+
+	if *debugFlag {
+		logLeveler.Set(slog.LevelDebug)
+	}
+
+	// initialize the application
+	app := &application.Application{
+		Name:       name,
+		Version:    version,
+		Controller: &application.Controller{},
+		Logger:     slog.New(logHandler),
+	}
+
+	slog.SetDefault(app.Logger)
+
+	app.Controller.Add("Environment", env.New("", map[string]string{}))
+	app.Controller.Add("Telemetry", otel.New())
+	app.Controller.Add("HTTP", http.New(content))
 
 	// if we have a configuration file, then pass it in to get parsed/processed
 	if *cfgFile != "" {
@@ -58,6 +86,9 @@ func HTTP(name, version string, content gohttp.FileSystem, opts ...application.O
 	for _, opt := range opts {
 		opt(app)
 	}
+
+	// add the application name to all logs on this logger
+	app.Logger = app.Logger.With("app", name)
 
 	// run the app
 	return app.Run(context.Background())
